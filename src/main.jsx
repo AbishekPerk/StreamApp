36437a5d5b3c457b4f8810b747f67089f27b0dc6
AbishekPerk <142099555+AbishekPerk@users.noreply.github.com>
2024-07-30 17:39:48 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename='/StreamApp/'>
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
   
